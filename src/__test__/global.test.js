d5a5999028536ffdf772460993fbbaa82f88472a
Alexis <dalexisbr@gmail.com>
2020-03-11 08:59:46 -0700

    InitialCommit

---
+++ src/__test__/global.test.js
@@ -0,0 +1,55 @@
+const text = 'Hola Mundo'
+const fruits = ['manzana','melon','banana']
+
+test('Debe contener un texto', () => {
+    expect(text).toMatch(/Hola/)
+})
+
+test('¿Tenemos una banana?', () => {
+    expect(fruits).toContain('banana')
+})
+
+test('Mayor que', () => {
+    expect(10).toBeGreaterThan(9)
+})
+
+test('verdadero', () => {
+    expect(true).toBeTruthy()
+})
+
+const reverseString = (str, callback) => {
+    callback(str.split("").reverse().join(""))
+}
+
+test('Probar Callback', () => {
+    reverseString('Hola', str => {
+        expect(str).toBe('aloH')
+    })
+})
+
+const reverseString2 = str => {
+    return new Promise((resolve, reject) => {
+        if (!str) {
+            reject(Error('Error'))
+        }
+        resolve(str.split("").reverse().join(""))
+    })
+}
+
+test('Probar promesa', () => {
+    return reverseString2('Hola')
+        .then(string => {
+            expect(string).toBe('aloH')
+        })
+})
+
+test('Probar Async/Await', async () => {
+    const string = await reverseString2('hola')
+    expect(string).toBe('aloh')
+})
+
+// afterEach( () => console.log('Despues de cada prueba'))
+// afterAll( () => console.log('Despues de todas las pruebas'))
+
+// beforeEach( () => console.log('Antes de cada prueba'))
+// beforeAll( () => console.log('Antes de todas las pruebas'))
